35981052f38d9b57fea687078c457d8715698d99
Meghesh10 <115342484+Meghesh10@users.noreply.github.com>
2022-10-19 13:54:48 +0530

    Add files via upload

---
+++ finding active url.py
@@ -0,0 +1,15 @@
+# # finding active urls
+import requests
+def request(url):
+    try:
+        return requests.get("https://" + url)
+        except requests.exceptions.ConnectionError:
+             pass
+target_url ="youtube.com"
+with open("/home/meghesh/subdomain.txt","r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = word + "." + target_url 
+        response = request(test_url)
+        if response:
+            print("[+]discovered subdomains -->  " + test_url)
